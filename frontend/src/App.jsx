b8706b8e091715a030af9d35be129e2ab692ab2c
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2025-01-04 12:00:00 +0530

    {commit_message}

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,37 @@ import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignup";
 import Captainlogin from "./pages/Captainlogin";
 import CaptainSignup from "./pages/CaptainSignup";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
+import UserLogout from "./pages/UserLogout";
 
 function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route path="/" element={<Start />} />
-          <Route path="/home" element={<Home />} />
-
           <Route path="/login" element={<UserLogin />} />
 
           <Route path="/signup" element={<UserSignup />} />
           <Route path="/captain-login" element={<Captainlogin />} />
           <Route path="/captain-signup" element={<CaptainSignup />} />
+
+          <Route path="/" element={<Start />} />
+          <Route
+            path="/home"
+            element={
+              <UserProtectWrapper>
+                <Home />
+              </UserProtectWrapper>
+            }
+          />
+          <Route
+            path="/user/logout"
+            element={
+              <UserProtectWrapper>
+                <UserLogout />
+              </UserProtectWrapper>
+            }
+          />
         </Routes>
       </div>
     </>
